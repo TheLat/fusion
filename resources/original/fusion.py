ae171b3174ee6a529d9a9fc40147fb3db0949709
Lat Ware <lat@pinterest.com>
2017-01-31 07:45:38 -0800

    Pokedex entries.

--- resources/original/fusion.py
+++ resources/original/fusion.py
@@ -20,6 +20,10 @@ while s != "":
         all[i]["TYPE1"] = s.split(" ")[1]
     if s.startswith("TYPE2: "):
         all[i]["TYPE2"] = s.split(" ")[1]
+    if s.startswith("DEX1: "):
+        all[i]["DEX1"] = s[6:]
+    if s.startswith("DEX2: "):
+        all[i]["DEX2"] = s[6:]
     if s.startswith("EXP_YIELD: "):
         all[i]["EXP_YIELD"] = int(s.split(" ")[1])
     if s.startswith("CATCHRATE: "):
@@ -94,6 +98,7 @@ while x <= len(all):
             f.write("\nTYPE2: %s" % all[str(y)]["TYPE2"])
         f.write("\nEXP_YIELD: %s" % ((all[str(x)]["EXP_YIELD"] + all[str(y)]["EXP_YIELD"])/2))
         f.write("\nCATCHRATE: %s" % ((all[str(x)]["CATCHRATE"] + all[str(y)]["CATCHRATE"])/2))
+        f.write("\nDEX: %s" % (all[str(x)]["DEX1"] + " " + all[str(y)]["DEX2"]))
         HP = str((int(all[str(x)]["HP"]) + int(all[str(y)]["HP"]))/2)
         f.write("\nHP: %s" % HP)
         ATTACK = str((int(all[str(x)]["ATTACK"]) + int(all[str(y)]["ATTACK"]))/2)
