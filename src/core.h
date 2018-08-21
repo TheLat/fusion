536af3aa2148aa951ffcaa3cf68431c2296807ef
TheLat <thouartaninny@gmail.com>
2018-08-21 01:37:33 -0700

    Minor optimization.

--- src/core.h
+++ src/core.h
@@ -5137,8 +5137,10 @@ public:
 	}
 	void draw_level() {
 		float xp, xl, yp, yl;
-		for (unsigned y = 0; y < levels[mc.loc.level].data.size(); ++y) {
-			for (unsigned x = 0; x < levels[mc.loc.level].data[y].size(); ++x) {
+		unsigned maxy = levels[mc.loc.level].data.size();
+		for (unsigned y = 0; y < maxy; ++y) {
+			unsigned maxx = levels[mc.loc.level].data[y].size();
+			for (unsigned x = 0; x < maxx; ++x) {
 				xp = -1.0f + (float(x) / 5.0f) - ((mc.loc.x - 4.5f) / 5.0f);
 				yp = (-float(y) / 4.5f) - (0.5f / 4.5f) + (mc.loc.y / 4.5f);
 				xl = 1.0f / 5.0f;
