cba33155ac62e481d33f641ef9e5ad69ad6f4814
Lat Ware <lat@pinterest.com>
2019-01-31 14:40:26 -0800

    Presentation mode improvement.

--- image-fusion/present.py
+++ image-fusion/present.py
@@ -12,17 +12,23 @@ widths, heights = zip(*(i.size for i in images))
 
 max_height = max(heights)
 max_width = 725
-new_im = Image.new('RGBA', (max_width, max_height))
+lines = 16
+new_im = Image.new('RGBA', (max_width, max_height*lines))
 
 xoffset = 0
+yoffset = 0
 for im in images:
     im = im.convert("RGBA")
     if xoffset + im.size[0] > max_width:
-        new_im.save("out2/%s.png" % count)
-        count = count + 1
-        new_im = Image.new('RGBA', (max_width, max_height))
+        if yoffset + max_height > new_im.size[1]:
+            new_im.save("out2/%s.png" % count)
+            count = count + 1
+            new_im = Image.new('RGBA', (max_width, max_height*lines))
+            yoffset = 0
+        else:
+            yoffset += max_height
         xoffset = 0
-    new_im.paste(im, (xoffset,0))
+    new_im.paste(im, (xoffset,yoffset))
     xoffset += im.size[0]
 
 new_im.save("out2/%s.png" % count)
