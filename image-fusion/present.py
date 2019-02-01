207635bac657ef30936f022b07c29bd33cb2e1c3
Lat Ware <lat@pinterest.com>
2019-01-31 16:25:49 -0800

    Bugfixes.

--- image-fusion/present.py
+++ image-fusion/present.py
@@ -3,24 +3,26 @@ from PIL import Image
 count = 0
 files = []
 
-for i in range(1,28):
-    for j in range(1,28):
+for i in range(1,29):
+    for j in range(1,29):
         files.append("out/%s-%s.png" % (i, j))
 
-images = map(Image.open, files)
+images = []
+for f in files:
+    images.append(Image.open(f).convert("RGBA"))
+
 widths, heights = zip(*(i.size for i in images))
 
 max_height = max(heights)
-max_width = 725
+max_width = 730
 lines = 16
 new_im = Image.new('RGBA', (max_width, max_height*lines))
 
 xoffset = 0
 yoffset = 0
 for im in images:
-    im = im.convert("RGBA")
-    if xoffset + im.size[0] > max_width:
-        if yoffset + max_height > new_im.size[1]:
+    if xoffset + im.size[0] >= max_width:
+        if yoffset + max_height >= new_im.size[1]:
             new_im.save("out2/%s.png" % count)
             count = count + 1
             new_im = Image.new('RGBA', (max_width, max_height*lines))
