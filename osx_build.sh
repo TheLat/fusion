994bcde1ef38c95aab5337e311b08059db9a182d
Lat Ware <lat@pinterest.com>
2019-06-11 12:48:31 -0700

    Partial commit.  OSX build command.

---
+++ osx_build.sh
@@ -0,0 +1,1 @@
+gcc src/*.cpp -I/usr/local/Cellar/libpng/1.6.37/include/libpng16 -L/usr/local/Cellar/libpng/1.6.37/lib -L'.' -lpng16 -framework OpenGL -framework GLUT -arch x86_64 -lfmod -lfmodL -lstdc++ -o fusion
