dcee524a0e3b4ca8d9d5308d83e93cf8bd2f72c7
TheLat <thouartaninny@gmail.com>
2019-12-24 12:58:14 -0800

    Creating needed data structure for custom keybinds.

--- src/input.cpp
+++ src/input.cpp
@@ -42,6 +42,14 @@ input::input() {
 	ifstream f("../windows_keybinds.dat");
 #endif
 	string line;
+	u_up = i_up;
+	u_down = i_down;
+	u_right = i_right;
+	u_left = i_left;
+	u_start = i_start;
+	u_select = i_select;
+	u_confirm = i_confirm;
+	u_cancel = i_cancel;
 	if (!f.is_open()) {
 		f.close();
 		return;
@@ -63,6 +71,36 @@ input::input() {
 	safe_getline(f, line);
 	i_select = stoi(line);
 	f.close();
+	u_up = i_up;
+	u_down = i_down;
+	u_right = i_right;
+	u_left = i_left;
+	u_start = i_start;
+	u_select = i_select;
+	u_confirm = i_confirm;
+	u_cancel = i_cancel;
+	ifstream f2("../user_keybinds.dat");
+	if (!f2.is_open()) {
+		f2.close();
+		return;
+	}
+	safe_getline(f2, line);
+	u_up = stoi(line);
+	safe_getline(f2, line);
+	u_down = stoi(line);
+	safe_getline(f2, line);
+	u_left = stoi(line);
+	safe_getline(f2, line);
+	u_right = stoi(line);
+	safe_getline(f2, line);
+	u_confirm = stoi(line);
+	safe_getline(f2, line);
+	u_cancel = stoi(line);
+	safe_getline(f2, line);
+	u_start = stoi(line);
+	safe_getline(f2, line);
+	u_select = stoi(line);
+	f2.close();
 }
 
 void input::tick() {
@@ -104,25 +142,25 @@ void input::tick() {
 }
 
 void input::keydown(bool &up, bool &down, bool &left, bool &right, bool &confirm, bool &cancel, bool &start, bool &select) {
-    up = key_down[i_up];
-    down = key_down[i_down];
-    left = key_down[i_left];
-    right = key_down[i_right];
-    confirm = key_down[i_confirm];
-    cancel = key_down[i_cancel];
-    start = key_down[i_start];
-    select = key_down[i_select];
+    up = key_down[i_up] || key_down[u_up];
+    down = key_down[i_down] || key_down[u_down];
+    left = key_down[i_left] || key_down[u_left];
+    right = key_down[i_right] || key_down[u_right];
+    confirm = key_down[i_confirm] || key_down[u_confirm];
+    cancel = key_down[i_cancel] || key_down[u_cancel];
+    start = key_down[i_start] || key_down[u_start];
+    select = key_down[i_select] || key_down[u_select];
 }
 
 void input::keypress(bool &up, bool &down, bool &left, bool &right, bool &confirm, bool &cancel, bool &start, bool &select) {
-    up = key_press[i_up];
-    down = key_press[i_down];
-    left = key_press[i_left];
-    right = key_press[i_right];
-    confirm = key_press[i_confirm];
-    cancel = key_press[i_cancel];
-    start = key_press[i_start];
-    select = key_press[i_select];
+    up = key_press[i_up] || key_press[u_up];
+    down = key_press[i_down] || key_press[u_down];
+    left = key_press[i_left] || key_press[u_left];
+    right = key_press[i_right] || key_press[u_right];
+    confirm = key_press[i_confirm] || key_press[u_confirm];
+    cancel = key_press[i_cancel] || key_press[u_cancel];
+    start = key_press[i_start] || key_press[u_start];
+    select = key_press[i_select] || key_press[u_select];
     for (unsigned i = 0; i < 16*16; ++i) {
         key_press[i] = false;
     }
--- src/input.h
+++ src/input.h
@@ -5,11 +5,12 @@ using namespace std;
 
 class input {
 private:
-    unsigned char keyMap[16];
-    bool key_down[16*16];
-    bool key_press[16*16];
+    unsigned char keyMap[16*2];
+    bool key_down[16*16*2];
+    bool key_press[16*16*2];
     unsigned time_index;
-    unsigned char i_up, i_down, i_left, i_right, i_confirm, i_cancel, i_start, i_select;
+	unsigned char i_up, i_down, i_left, i_right, i_confirm, i_cancel, i_start, i_select;
+	unsigned char u_up, u_down, u_left, u_right, u_confirm, u_cancel, u_start, u_select;
 public:
     void keydown(bool &up, bool &down, bool &left, bool &right, bool &confirm, bool &cancel, bool &start, bool &select);
     void keypress(bool &up, bool &down, bool &left, bool &right, bool &confirm, bool &cancel, bool &start, bool &select);
