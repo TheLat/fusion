7b9ea7cbea2ce6a27390ac22c84f21bba6a0fc9c
TheLat <thouartaninny@gmail.com>
2020-01-06 15:15:55 -0800

    Partial commit.  Controller.

--- src/input.cpp
+++ src/input.cpp
@@ -7,8 +7,21 @@
 #ifdef __APPLE__
 #include <Carbon/Carbon.h>
 #else
-#include <Windows.h>
+#include <windows.h>
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#include <dinputd.h>
+LPDIRECTINPUT8          g_pDI = NULL;
+LPDIRECTINPUTDEVICE8    g_pJoystick = NULL;
+struct DI_ENUM_CONTEXT
+{
+	DIJOYCONFIG* pPreferredJoyCfg;
+	bool bPreferredJoyCfgValid;
+};
+
 #endif
+
+bool use_controller = false;
 
 extern bool safe_getline(ifstream &f, string& s);
 timer tim2;
@@ -107,6 +120,10 @@ input::input() {
 		u_select = stoi(line);
 		f2.close();
 	}
+#ifdef __APPLE__
+#else
+	init_controller();
+#endif
 }
 
 void input::tick() {
@@ -139,6 +156,124 @@ void input::tick() {
         key_down[(i*16) + 7] = (keyMap[i] & 128) != 0;
     }
 #else
+	if (use_controller) {
+		HRESULT hr;
+		TCHAR strText[512] = { 0 }; // Device state text
+		DIJOYSTATE2 cs;           // DInput controller state 
+		hr = g_pJoystick->Acquire();
+		hr = g_pJoystick->Poll();
+		if (FAILED(hr)) {
+			hr = g_pJoystick->Acquire();
+		}
+		else {
+			if (FAILED(hr = g_pJoystick->GetDeviceState(sizeof(DIJOYSTATE2), &cs))) {
+				printf("\nUnable to get controller state.");
+			}
+			else {
+				if (FAILED(g_pJoystick->GetDeviceState(sizeof(DIJOYSTATE2), &cs))) {
+					printf("\nUnable to get controller state.");
+				}
+				else {
+					controller[0] = cs.lX;
+					controller[1] = cs.lY;
+					controller[2] = cs.lZ;
+					controller[3] = cs.lRx;
+					controller[4] = cs.lRy;
+					controller[5] = cs.lRz;
+					controller[6] = cs.rglSlider[0];
+					controller[7] = cs.rglSlider[1];
+					controller[8] = cs.lVX;
+					controller[9] = cs.lVY;
+					controller[10] = cs.lVZ;
+					controller[11] = cs.lVRx;
+					controller[12] = cs.lVRy;
+					controller[13] = cs.lVRz;
+					controller[14] = cs.rglVSlider[0];
+					controller[15] = cs.rglVSlider[1];
+					controller[16] = cs.lAX;
+					controller[17] = cs.lAY;
+					controller[18] = cs.lAZ;
+					controller[19] = cs.lARx;
+					controller[20] = cs.lARy;
+					controller[21] = cs.lARz;
+					controller[22] = cs.rglASlider[0];
+					controller[23] = cs.rglASlider[1];
+					controller[24] = cs.lFX;
+					controller[25] = cs.lFY;
+					controller[26] = cs.lFZ;
+					controller[27] = cs.lFRx;
+					controller[28] = cs.lFRy;
+					controller[29] = cs.lFRz;
+					controller[30] = cs.rglFSlider[0];
+					controller[31] = cs.rglFSlider[1];
+					controller[32] = cs.rgdwPOV[0];
+					if (controller[32] != -1) {
+						switch (controller[32]) {
+						case 0:
+							controller[32] = 1000;
+							controller[33] = 0;
+							controller[34] = 0;
+							controller[35] = 0;
+							break;
+						case 4500:
+							controller[32] = 1000;
+							controller[33] = 1000;
+							controller[34] = 0;
+							controller[35] = 0;
+							break;
+						case 9000:
+							controller[32] = 0;
+							controller[33] = 1000;
+							controller[34] = 0;
+							controller[35] = 0;
+							break;
+						case 13500:
+							controller[32] = 0;
+							controller[33] = 1000;
+							controller[34] = 1000;
+							controller[35] = 0;
+							break;
+						case 18000:
+							controller[32] = 0;
+							controller[33] = 0;
+							controller[34] = 1000;
+							controller[35] = 0;
+							break;
+						case 22500:
+							controller[32] = 0;
+							controller[33] = 0;
+							controller[34] = 1000;
+							controller[35] = 1000;
+							break;
+						case 27000:
+							controller[32] = 0;
+							controller[33] = 0;
+							controller[34] = 0;
+							controller[35] = 1000;
+							break;
+						case 31500:
+							controller[32] = 1000;
+							controller[33] = 0;
+							controller[34] = 0;
+							controller[35] = 1000;
+							break;
+						default:
+							break;
+						}
+					}
+					else {
+						controller[32] = 0;
+						controller[33] = 0;
+						controller[34] = 0;
+						controller[35] = 0;
+					}
+					for (unsigned i = 0; i < 128; ++i) {
+						controller[36 + i] = cs.rgbButtons[i] ? 1000 : 0;
+					}
+				}
+			}
+		}
+	}
 	for (unsigned i = 0; i < 16 * 16; ++i) {
 		if (!key_down[i] && GetAsyncKeyState(i) != 0)
 			key_press[i] = true;
@@ -359,4 +494,117 @@ void input::save_bindings() {
 	f << int(u_start) << string("\n");
 	f << int(u_select);
 	f.close();
+}
+
+#ifdef __APPLE__
+#else
+BOOL CALLBACK EnumJoysticksCallback(const DIDEVICEINSTANCE* pdidInstance,
+	VOID* pContext)
+{
+	DI_ENUM_CONTEXT* pEnumContext = (DI_ENUM_CONTEXT*)pContext;
+	HRESULT hr;
+
+	// Skip anything other than the perferred Joystick device as defined by the control panel.  
+	// Instead you could store all the enumerated Joysticks and let the user pick.
+	if (pEnumContext->bPreferredJoyCfgValid &&
+		!IsEqualGUID(pdidInstance->guidInstance, pEnumContext->pPreferredJoyCfg->guidInstance))
+		return DIENUM_CONTINUE;
+
+	// Obtain an interface to the enumerated Joystick.
+	hr = g_pDI->CreateDevice(pdidInstance->guidInstance, &g_pJoystick, NULL);
+
+	// If it failed, then we can't use this Joystick. (Maybe the user unplugged
+	// it while we were in the middle of enumerating it.)
+	if (FAILED(hr))
+		return DIENUM_CONTINUE;
+
+	// Stop enumeration. Note: we're just taking the first Joystick we get. You
+	// could store all the enumerated Joysticks and let the user pick.
+	return DIENUM_STOP;
+}
+
+BOOL CALLBACK EnumObjectsCallback(const DIDEVICEOBJECTINSTANCE* pdidoi,
+	VOID* pContext)
+{
+	HWND hDlg = (HWND)pContext;
+
+	static int nSliderCount = 0;  // Number of returned slider controls
+	static int nPOVCount = 0;     // Number of returned POV controls
+
+								  // For axes that are returned, set the DIPROP_RANGE property for the
+								  // enumerated axis in order to scale min/max values.
+	if (pdidoi->dwType & DIDFT_AXIS)
+	{
+		DIPROPRANGE diprg;
+		diprg.diph.dwSize = sizeof(DIPROPRANGE);
+		diprg.diph.dwHeaderSize = sizeof(DIPROPHEADER);
+		diprg.diph.dwHow = DIPH_BYID;
+		diprg.diph.dwObj = pdidoi->dwType; // Specify the enumerated axis
+		diprg.lMin = -1000;
+		diprg.lMax = +1000;
+
+		// Set the range for the axis
+		if (FAILED(g_pJoystick->SetProperty(DIPROP_RANGE, &diprg.diph)))
+			return DIENUM_STOP;
+
+	}
+	return DIENUM_CONTINUE;
+}
+
+#endif
+
+void input::init_controller() {
+#ifdef __APPLE__
+#else
+	HRESULT hr;
+	if (FAILED(hr = DirectInput8Create(GetModuleHandle(NULL), DIRECTINPUT_VERSION, IID_IDirectInput8, (VOID**)&g_pDI, NULL))) {
+		printf("Unable to create DirectInput8 device.  Contining without controller.\n");
+		return;
+	}
+
+	DIJOYCONFIG PreferredJoyCfg = { 0 };
+	DI_ENUM_CONTEXT enumContext;
+	enumContext.pPreferredJoyCfg = &PreferredJoyCfg;
+	enumContext.bPreferredJoyCfgValid = false;
+
+	IDirectInputJoyConfig8* pJoyConfig = NULL;
+	if (FAILED(hr = g_pDI->QueryInterface(IID_IDirectInputJoyConfig8, (void**)&pJoyConfig))) {
+		printf("Unable to query interface.  Continuing without controller.\n");
+		return;
+	}
+	PreferredJoyCfg.dwSize = sizeof(PreferredJoyCfg);
+	if (SUCCEEDED(pJoyConfig->GetConfig(0, &PreferredJoyCfg, DIJC_GUIDINSTANCE)))
+		enumContext.bPreferredJoyCfgValid = true;
+	if (pJoyConfig) {
+		pJoyConfig->Release();
+		pJoyConfig = NULL;
+	}
+	if (FAILED(hr = g_pDI->EnumDevices(DI8DEVCLASS_GAMECTRL, EnumJoysticksCallback, &enumContext, DIEDFL_ALLDEVICES))) {
+		printf("Unable to enumerate devices.  Continuing without controller.\n");
+		return;
+	}
+	if (g_pJoystick == NULL) {
+		printf("Unable to find controller.  Continuing without controller.\n");
+		return;
+	}
+	if (FAILED(g_pJoystick->SetDataFormat(&c_dfDIJoystick2))) {
+		printf("Unable to set controller format.  Continuing without controller.\n");
+		return;
+	}
+	if (FAILED(g_pJoystick->EnumObjects(EnumObjectsCallback, (VOID*)0, DIDFT_ALL))) {
+		printf("Unable to enumerate controller's objects.  Continuing without controller.\n");
+		return;
+	}
+	printf("Controller detected.\n");
+	// TODO: Controller input bindings
+	use_controller = true;
+#endif
+}
+
+input::~input() {
+#ifdef __APPLE__
+#else
+	if (use_controller)
+		g_pJoystick->Unacquire();
+#endif
 }
--- src/input.h
+++ src/input.h
@@ -8,9 +8,11 @@ private:
     unsigned char keyMap[16*2];
     bool key_down[16*16*2];
     bool key_press[16*16*2];
+	int controller[164];
     unsigned time_index;
 	unsigned char i_up, i_down, i_left, i_right, i_confirm, i_cancel, i_start, i_select;
 	unsigned char u_up, u_down, u_left, u_right, u_confirm, u_cancel, u_start, u_select;
+	int c_up, c_down, c_left, c_right, c_confirm, c_cancel, c_start, c_select;
 public:
 	unsigned char get_pressed_key(bool& pressed);
 	bool set_key(unsigned char key, int mapping);
@@ -18,7 +20,9 @@ public:
     void keypress(bool &up, bool &down, bool &left, bool &right, bool &confirm, bool &cancel, bool &start, bool &select);
     void tick();
 	void save_bindings();
+	void init_controller();
 	input();
+	~input();
 };
 
 #endif
