85b16839448788f379f723be1f254b4c73ee86a7
Lat Ware <lat.ware@affirm.com>
2020-01-11 21:03:24 -0800

    Adding install requirement.

--- osx_build.sh
+++ osx_build.sh
@@ -1,3 +1,4 @@
+brew install libpng
 rm Release/fusion
 rm fusion
 gcc src/*.cpp -I/usr/local/Cellar/libpng/1.6.37/include/libpng16 -L/usr/local/Cellar/libpng/1.6.37/lib -L'.' -lpng16 -framework OpenGL -framework GLUT -arch x86_64 -lfmod -lfmodL -lstdc++ -framework carbon -o fusion
